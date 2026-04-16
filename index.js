2eb8cccc487bd6aa73752ea78a424670dc706c70
moatassem47 <199459806+moatassem47@users.noreply.github.com>
2026-04-17 01:26:54 +0200

    Add simple GUI

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const results = document.querySelector("#results");
+const choices = document.querySelector("#choices");
+const winner = document.querySelector("#winner");
 let humanScore = 0;
 let computerScore = 0;
 let numberOfRounds = 0;
@@ -60,23 +63,45 @@ function playRound(humanChoice, computerChoice) {
     console.log(`human's score = ${humanScore}`);
     console.log(`computer's score = ${computerScore}`);
   }
+  results.textContent = ` human's score = ${humanScore} -------- computer's score = ${computerScore}`;
+  choices.textContent = `human choice = ${humanChoice} -------- computer's choice = ${computerChoice}`;
+  if (humanScore == 5) {
+    winner.textContent = `winner is human`;
+    return;
+  }
+  if (computerScore == 5) {
+    winner.textContent = `winner is computer`;
+    return;
+  }
 }
 
-function playGame() {
-  for (i = 0; i < 5; i++) {
-    console.log("=================================");
-    hChoice = getHumanChoice();
-    cChoice = computerInput();
-    console.log(`human choice is ${hChoice}  `);
-    console.log(`computer choice is ${cChoice}  `);
-    playRound(hChoice, cChoice);
-  }
-  if (computerScore > humanScore) {
-    console.log("COMPUTER WINS!!");
-  } else if (computerScore < humanScore) {
-    console.log("HUMAN WINS!");
-  } else {
-    console.log("IT'S A DRAW!");
-  }
-}
-playGame();
+// function playGame() {
+//   for (i = 0; i < 5; i++) {
+//     console.log("=================================");
+//     hChoice = getHumanChoice();
+//     cChoice = computerInput();
+//     console.log(`human choice is ${hChoice}  `);
+//     console.log(`computer choice is ${cChoice}  `);
+//     playRound(hChoice, cChoice);
+//   }
+//   if (computerScore > humanScore) {
+//     console.log("COMPUTER WINS!!");
+//   } else if (computerScore < humanScore) {
+//     console.log("HUMAN WINS!");
+//   } else {
+//     console.log("IT'S A DRAW!");
+//   }
+// }
+// playGame();
+const paperBtn = document.querySelector("#paperBtn");
+const rockBtn = document.querySelector("#rockBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+paperBtn.addEventListener("click", () => {
+  playRound("paper", computerInput());
+});
+rockBtn.addEventListener("click", () => {
+  playRound("rock", computerInput());
+});
+scissorsBtn.addEventListener("click", () => {
+  playRound("scissors", computerInput());
+});
